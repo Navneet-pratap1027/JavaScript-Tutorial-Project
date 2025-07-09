a84beae21de426ddc921d146533fb26ca60038e4
NAVNEET PRATAP <pratap102718@gmail.com>
2025-07-09 04:44:18 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,42 @@
 let score ="33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN(not a number)
 // true => 1; false => 0; 
 
-let isLoggedIn =1
-let booleanIsLoggedIn = Bollean(isLoggedIn)
+//let isLoggedIn =1
+//let booleanIsLoggedIn = Bollean(isLoggedIn)
+
+// ########### OPERATION #################//
+ let value =3
+  let negValue=-value
+  //console.log(negValue);
+  //console.log(2+2);
+  let str1 = "hello"
+  let str2 = " Navneet"
+  let str3 = str1+str2
+ // console.log(str3); 
+ // console.log("1" + 2);
+ // console.log("1" + "2");
+ // console.log(1 + "2");
+ // console.log( 1+ 2);
+ // console.log("1"+ 5+2);
+ // console.log(1 + 5+ "2");
+
+// console.log(+true);
+// console.log(+"");
+  let num1, num2, num3
+
+  num1= num2= num3=2+2
+
+  let gameCounter=100
+  gameCounter++;
+//  console.log(gameCounter);
