5a1a117f706166b1906b1d4490cccca6b149a550
NAVNEET PRATAP <pratap102718@gmail.com>
2025-07-09 10:53:00 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name ="Navneet"
+const repoCount = 10
+
+// console.log(name + repoCount + " value");  (outdated way)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
