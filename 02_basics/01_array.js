046db1edeeb8042ab51389928868ec79e9b217f4
NAVNEET PRATAP <pratap102718@gmail.com>
2025-07-13 02:52:54 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,27 @@
+// ARRAY
+
+const myArr = [2,3,4,5,6,7,8]
+const myFruits = ["apple","banana","graphes","pineapple"]
+
+const myArr2 =new Array(6,5,4,2,3,6,8)
+// console.log(myArr[4]);
+
+// ARRAY METHODS
+
+// myArr.push(9)
+// myArr.push(10)
+// myArr.pop()
+
+// myArr.unshift(1)// (add any element in starting position of array)
+// myArr.shift()
+
+
+// console.log(myArr.includes(18));// (to check this element is included or not in the array)
+// console.log(myArr.indexOf(4));
+
+// const newArr =myArr.join()// add all element and convert it into string
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// SLICE , SPLICE
