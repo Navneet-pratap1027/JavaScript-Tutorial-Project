f8f3827bf26783a673042b790d5b52101c8799b9
NAVNEET PRATAP <pratap102718@gmail.com>
2025-07-15 12:12:50 +0000

    json talk

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -28,3 +28,56 @@ const obj3 = {5: "g", 6: "e"}
 // const obj4 = Object.assign({}, obj1,obj2,obj3)
 const obj4 = {...obj1, ...obj2}
 // console.log(obj4);
+
+const users = [
+    {
+    id: 1, 
+    email: "awesome@gmail.com"
+},
+{
+    id: 1, 
+    email: "awesome@gmail.com"
+},
+{
+    id: 1, 
+    email: "awesome@gmail.com"
+},
+]
+
+// users[1].email
+
+// console.log(instaUser);
+
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+
+
+// console.log(instaUser.hasOwnProperty('isLoggedIn'));
+
+const course ={
+    coursename: "Js in Hindi",
+    price:"999",
+    courseInstructor: "Hitesh"
+}
+
+// console.log(course.price);
+
+const {courseInstructor: instructor} = course// distructure
+// console.log(courseInstructor);
+console.log(instructor);
+
+// Json and API
+
+// {
+//    "name": "Navneet",
+//    "coursename": "Js in hindi",
+//    "price": "free"
+// }
+
+[
+    {},
+    {},
+{}
+]
+
