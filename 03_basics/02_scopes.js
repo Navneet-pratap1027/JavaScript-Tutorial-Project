318cc3237f6664f16f38e8b57bf425e29cf6bbf5
NAVNEET PRATAP <pratap102718@gmail.com>
2025-07-18 10:57:16 +0000

    scoopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+// var c = 400
+let a= 100
+if (true){
+    let a = 10
+const  b = 20
+console.log("INNER: ",a);
+// var c = 60//why we are not using var
+
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// difference between global and block scoopes
