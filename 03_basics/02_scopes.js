67efd7d9aacdf4776a31cfe49acfb684b59be21a
NAVNEET PRATAP <pratap102718@gmail.com>
2025-07-20 03:41:57 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,13 +3,54 @@ let a= 100
 if (true){
     let a = 10
 const  b = 20
-console.log("INNER: ",a);
+// console.log("INNER: ",a);
 // var c = 60//why we are not using var
 
 }
 
-console.log(a);
+//console.log(a);
 // console.log(b);
 // console.log(c);
 
 // difference between global and block scoopes
+
+function one(){
+    const username = "Navneet"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+   // console.log(website);
+
+    two()
+}
+
+//one()
+
+if (true) {
+    const username = "Navneet"
+    if (username === "Navneet"){
+        const website = " youtube"
+       // console.log(username+website);
+    }
+
+    // console.log(website);// outside the scopes
+}
+
+// console.log(username); // outside the scopes
+
+
+// ###################  INTERESTING   ######################
+console.log(addone(5))// here in this type function decleartion we can return function here also..
+function addone(num){
+    return num + 1
+}
+
+addtwo(5)
+const addtwo = function(num){  // here in this type function decleartion we can not return function here it reflect error because we store that function inside  a variable..
+    return num + 2
+}
+
+
