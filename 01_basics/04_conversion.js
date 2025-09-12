be78a4e9a67e7a6f38e3cc06c0b413c0760c48eb
NAVNEET PRATAP <pratap102718@gmail.com>
2025-09-12 11:11:21 +0000

    updated

--- 01_basics/04_conversion.js
+++ 01_basics/04_conversion.js
@@ -2,18 +2,21 @@
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 == 1);
-//console.log(2 != 1);
+// console.log(2 != 1);
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("3" > "5");// ascii code comparesion
+// console.log("02" > 1);// string converted into number then comapre
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);
-console.log(undefined ==0);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null === 0);//here null as object type
+console.log(null >= 0);
+console.log(null == undefined);//losely comapre always true(i.e compiler does not change data types)
+console.log(null ===undefined);// strict comapre( )
+console.log(undefined ==0);//only undefined=null true ,rest all are false
 console.log(undefined < 0);
 console.log(undefined > 0);
 
 //===(tripple equal, also check datatype of given value)
 
-console.log("2"=== 2);
+// console.log("2"=== 2);
