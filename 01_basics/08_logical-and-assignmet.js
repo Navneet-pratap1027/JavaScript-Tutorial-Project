cad3be3c5a3995bf68318ba5d444be284579a162
NAVNEET PRATAP <pratap102718@gmail.com>
2025-09-12 11:41:22 +0000

    logical

---
+++ 01_basics/08_logical-and-assignmet.js
@@ -0,0 +1,29 @@
+// && => and
+// || => or
+// ! => reverse
+
+let isLoggedin =true;
+let isPaid = true;
+
+// console.log(isLoggedin && isPaid);
+
+let isEmailuser = true;
+let isGoogleuser =false;
+
+// console.log(isEmailuser || isGoogleuser);
+
+// ####  ASSIGNMENT #####
+
+
+let num1 = 10;
+
+// num1 = num1 + 5
+num1 += 5;
+
+ console.log(num1);
+
+ //let score = 10 + 22 *4 - 6// bad practice 
+
+
+let score = ((10 +( 22 *4) )- 6)
+ console.log(score);
