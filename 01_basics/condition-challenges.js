c3211349e7a456b1038d0ea551062e905310be7e
NAVNEET PRATAP <pratap102718@gmail.com>
2025-09-13 07:09:10 +0000

    challenges

---
+++ 01_basics/condition-challenges.js
@@ -0,0 +1,51 @@
+let num1 = 5;
+let num2 = 4;
+/*
+console.log("I am reqular upper code");
+
+if (num1 < num2){
+    console.log("Num 1 is greater than num2");
+}
+else {
+    console.log("Num 1 is less than num2");
+}
+
+console.log("I am reqular upper code");
+*/
+// Checking if a string is equal to another sting:
+/*
+let username = "Navneet";
+let anotherusername = "Navneet";
+if (username==anotherusername){
+    //console.log("Pick another username");
+}else{
+    console.log("You can pick this username");
+}
+
+// Checking if a variable is a number or not:
+
+let score = "22";
+if(typeof score==="number"){
+    console.log("yes, this is a number");
+}else{
+    console.log("NO, this is not a number");
+}
+*/
+// Checking if a boolean value is true or false:
+
+let isTeaReady = false
+
+if(isTeaReady){
+    console.log("Tea is ready");
+}else{
+    console.log("Tea is not ready")
+}
+
+// checking arry is empty or not
+let items=[];
+console.log(items.length);
+if(items.length===0){
+    console.log("Array is empty");
+}else{
+    console.log("Array is not empty");
+}
