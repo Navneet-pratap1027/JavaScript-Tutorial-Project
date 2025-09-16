0fd2abde8f47fbb7c8a25959facb555396801125
NAVNEET PRATAP <pratap102718@gmail.com>
2025-09-16 07:00:52 +0000

    FUNCTION

---
+++ 03_basics/05_functions.js
@@ -0,0 +1,75 @@
+/* 
+1. Write a function named makeTea that takes one parameter, type0fTea, and returns a string like "Making green tea" when called with "green tea".
+Store the result in a variable named tea0rder. */
+
+function makeTea(type0fTea){
+    return `Making ${type0fTea}`;
+}
+let teaOrder=makeTea("green tea"); 
+// console.log(teaOrder)
+
+
+/* 
+2. Create a function named orderTea that takes one parameter, teaType. Inside this function, create another function named confirmOrder that returns a message like "Order confirmed for chai".Call confirmOrder from within orderTea`and return the result. */
+/* 
+function orderTea(teaType){
+    function confirmOrder(){
+        return `Order confirmed for ${teaType}`;
+    }
+    return confirmOrder()
+} */
+//let orderConfirmation = orderTea("chai");// 
+// console.log(orderTea("chai"));
+
+
+
+
+//   ==>>  ################### THIS AND ARROW FUNCTION ##################3
+/* 
+const user ={
+    username: "Navneet",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        // console.log(this);
+    }
+}
+
+user.welcomeMessage()
+user.username ="sam"
+user.welcomeMessage()
+//  console.log(this);
+
+/* function chai(){
+    let username = "Navneet"
+    console.log(this.username);
+}
+chai()
+ */
+
+ const addTwo =(num1,num2) =>{
+    return num1+num2
+    
+} 
+// console.log(addTwo(6,7))
+ 
+
+/*
+3. Write an arrow function named calculateTotal that takes two parameters: price and quantity. The function should return the total cost by multiplying the price and quantity.Store the result in a variable named totalCost.
+*/
+// const calculateTotal = ( price,quantity)=>price*quantity // (explicit way of writting in one line)
+    
+// const calculateTotal = ( price,quantity)=>{
+//     return price*quantity
+
+}
+let totalCost= calculateTotal(399 ,10)
+console.log(totalCost);
+
+
+
+/* 
+4. Write a function named processTeaOrder that takes another function, makeTea makeTea, as a parameter and calls it with the argument "earl grey".Return the result of calling makeTea. */
+
+
