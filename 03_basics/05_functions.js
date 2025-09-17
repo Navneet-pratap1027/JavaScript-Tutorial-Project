bca1f057c71da6030fdad42d2935034e1030cf02
NAVNEET PRATAP <pratap102718@gmail.com>
2025-09-17 10:55:00 +0000

    function

--- 03_basics/05_functions.js
+++ 03_basics/05_functions.js
@@ -49,6 +49,7 @@ chai()
  */
 
  const addTwo =(num1,num2) =>{
+   
     return num1+num2
     
 } 
@@ -60,8 +61,8 @@ chai()
 */
 // const calculateTotal = ( price,quantity)=>price*quantity // (explicit way of writting in one line)
     
-// const calculateTotal = ( price,quantity)=>{
-//     return price*quantity
+const calculateTotal = ( price,quantity)=>{
+    return price*quantity
 
 }
 let totalCost= calculateTotal(399 ,10)
@@ -72,4 +73,49 @@ console.log(totalCost);
 /* 
 4. Write a function named processTeaOrder that takes another function, makeTea makeTea, as a parameter and calls it with the argument "earl grey".Return the result of calling makeTea. */
 
+// Pehle ek function banate hain jo chai banata hai
+function makeTea(typeOfTea) {
+    return `Making ${typeOfTea}`;
+}
 
+// Ab ek function jo makeTea ko parameter ke roop me accept kare
+function processTeaOrder(makeTea) {
+    // "earl grey" pass karke call karte hain
+    return makeTea("earl grey");
+}
+
+// Call kar ke dekhte hain
+let order = processTeaOrder(makeTea);
+console.log(order); // Output: Making earl grey
+
+/*
+5. Write a function named createTeaMaker that returns another function. The returned function should take one parameter, teaType, and return a message like "Making green tea".Store the returned function in a variable named teaMaker and call it with "green tea".
+*/
+
+function createTeaMaker(){
+    return function(teaType){
+        return `Making ${teaType}`;
+    }
+}
+let teaMaker= createTeaMaker()
+
+console.log(teaMaker("greentea"));
+
+
+
+/* function createTeaMaker(name) {
+  let score = 100;   // ek local variable
+
+  return function (teaType) {
+    return Making ${teaType} ${name} ${score};
+  };
+}
+
+
+let teaMaker = createTeaMaker("Navneet");  
+let result = teaMaker("green tea");
+
+console.log(result); */  
+
+
+/* Jab ek inner function apne outer function ke variables ko yaad rakhta hai aur unhe baad me use kar sakta hai, chahe outer function return ho chuka ho   => CLOSURE BEHAVIOUR*/
