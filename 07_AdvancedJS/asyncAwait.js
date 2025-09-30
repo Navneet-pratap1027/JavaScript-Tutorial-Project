30457e59b2f0afd4ba477e0e294104172f418040
Navneet Pratap <pratap102718@gmail.com>
2025-10-01 00:32:17 +0530

    asynAwait

--- 07_AdvancedJS/asyncAwait.js
+++ 07_AdvancedJS/asyncAwait.js
@@ -1,4 +1,4 @@
-/* function fetchUserData(){
+function fetchUserData(){
     return new Promise((resolve, reject)=>{
         setTimeout(() =>{
             resolve({name: "chaicode", url: "https://chaicode.com"})
@@ -11,12 +11,12 @@ async function getUserData(){
         console.log('Fetching user data..');
         const userdata = await fetchUserData()
         console.log("User data fetched succesfully");
-        console.log("User data",userdata);
+        console.log("User data: ",userdata);
     }catch(error){
         console.log("Errror fetching data",error);
     }
 }
-getUserData(); */
+getUserData();
 
  function fetchPostData(){
     return new Promise((resolve)=>{
@@ -35,12 +35,15 @@ getUserData(); */
  }
 async function getBlogData(){
     try {
-        console.log("Fetching complete data");
-        const blogData = await fetchPostData();
-        const commonData = await fetchCommonData();
+        console.log("Fetching blog data");
+        // const blogData = await fetchPostData();
+        // const commonData = await fetchCommonData();
+       const [postData,commonData]= await Promise.all([fetchPostData(),fetchCommonData()])
+        console.log(blogData);
+        console.log(commonData);
         console.log("Fetch completed ");
     } catch (error) {
         console.error("Error fetching blog data", error);
     }
 }
-getBlogData();// node 07_AdvancedJS/asyncAwait.js
+getBlogData();// node 07_AdvancedJS/asyncAwait.js 
