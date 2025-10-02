b1b78b904a43ca35cf9208d3966a1cd7f0acd384
Navneet Pratap <pratap102718@gmail.com>
2025-10-02 11:21:25 +0530

    completed advanced

---
+++ 07_AdvancedJS/appM.js
@@ -0,0 +1,7 @@
+// Module import file
+
+// default import
+import multiply from "./mathOperationsM.js";
+// named import 
+import{add,subtract}from "./mathOperationsM.js"
+console.log(multiply(2,2));
